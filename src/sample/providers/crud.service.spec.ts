4146022377887319f3e65a40db2e61314eb23a64
YAro Script <yar.yason@gmail.com>
2023-03-13 23:53:56 +0200

    Initial commit

---
+++ src/sample/providers/crud.service.spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import type { DataSourceOptions } from 'typeorm';
+
+import { Sampletable1 } from '#entity/sampledb1';
+import { CrudService } from './crud.service';
+import { configuration } from '../../config';
+
+let moduleRef: TestingModule | undefined;
+let crud: CrudService;
+let idx: number;
+
+beforeAll(async () => {
+  moduleRef = await Test.createTestingModule({
+    imports: [
+      TypeOrmModule.forRoot({ ...(<DataSourceOptions>(await configuration()).db) }),
+      TypeOrmModule.forFeature([Sampletable1]),
+    ],
+    providers: [CrudService],
+  }).compile();
+
+  crud = moduleRef.get(CrudService);
+});
+
+test('create', async () => {
+  const result = await crud.create({ title: 'FooBar', content: 'Hello World', tags: ['new'] });
+  expect(result).toHaveProperty('id');
+  idx = result.id;
+});
+
+test('read', async () => {
+  expect(await crud.read(idx)).toBeInstanceOf(Sampletable1);
+});
+
+test('update', async () => {
+  expect(await crud.update(idx, { title: 'Blahblahblah', tags: ['update'] })).toHaveProperty('affected');
+});
+
+test('delete', async () => {
+  const result = await crud.remove(idx);
+  expect(result).toHaveProperty('affected');
+  expect(result.affected).toBe(1);
+});
+
+afterAll(async () => {
+  await moduleRef?.close();
+});
